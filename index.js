85905951aa864434b543a5a48d0c24a81aa68c63
msv-karthik <msvkarthik12@gmail.com>
2024-11-09 00:43:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick= () =>{
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
+}; 
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick= () =>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}; 
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick= () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}; 
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let theme = document.querySelector('#color-mode');
+
+theme.onclick = () =>{
+    theme.classList.toggle('fa-sun');
+
+    if(theme.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }
+    else
+    {
+        document.body.classList.remove('active');
+    }
+};
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
